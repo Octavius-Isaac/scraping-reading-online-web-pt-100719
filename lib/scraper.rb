802ea39130bd410f893c371b108ba85a697cc9af
Solomon Octavius Isaac <octavius.isaac@gmail.com>
2019-11-18 08:07:04 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,14 @@
+#require Nokogiri and open-uri
 require 'nokogiri'
 require 'open-uri'
 
+#Grab HTML from landing page
 html = open("https://flatironschool.com/")
+
+#Take string of HTML returned from above and convert to a Node Set
+doc = Nokogiri::HTML(html)
+courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+ 
+courses.each do |course|
+  puts course.text.strip
+end
